cb62b2b682289c419067473337a1f3dde15cc593
Jean-Baptiste DEMONTE <jbdemonte@gmail.com>
2021-10-08 11:18:57 +0200

    style system icons

--- src/components/System.tsx
+++ src/components/System.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, styled } from '@mui/material';
 import systems from '../data/systems.json';
 import { ISystem } from '../interfaces/system.interface';
 
@@ -6,19 +6,55 @@ type Props = {
   system: string;
 }
 
+const StyledBox = styled(Box)`
+  width: 100px;
+  background-color: transparent;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  div {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 3px;
+    margin-bottom: 2px;
+  }
+  
+  img {
+    height: 32px;
+  }
+  
+  p {
+    font-size: 12px;
+    text-align: center;
+    margin: 0;
+    border-radius: 3px;
+  }
+  
+  &:hover {
+    p, div {
+      background: #414244;
+    }
+  }
+`;
+
 export const System = ({ system: id }: Props) => {
   const system = (systems as ISystem[]).find(system => system.id === id);
 
   return (
-    <Box sx={{ width: 100, backgroundColor: 'transparent', textAlign: 'center'}}>
-      <img
-        height="32"
-        src={`${process.env.PUBLIC_URL}/systems/icons/${system?.icon || 'missing.png'}`}
-        alt={system?.name || id}
-      />
-      <Typography sx={{ fontSize: 12, textAlign: 'center'}}>
+    <StyledBox>
+      <div>
+        <img
+          src={`${process.env.PUBLIC_URL}/systems/icons/${system?.icon || 'missing.png'}`}
+          alt={system?.name || id}
+        />
+      </div>
+      <p>
         { system?.name || id }
-      </Typography>
-    </Box>
+      </p>
+    </StyledBox>
   )
 }
